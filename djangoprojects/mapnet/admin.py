df154fc54b2fa2f2294148fa65a0b800cdbc364b
giorgi2003 <khachidze.giorgi2003@gmail.com>
2026-02-02 10:39:26 +0100

    asda

--- djangoprojects/mapnet/admin.py
+++ djangoprojects/mapnet/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.     
 
-from mapnet.views import register  # Importiere die register-Ansicht    
-from mapnet.views import login # Importiert die Login-Ansicht
+
+# from mapnet.views import login 
